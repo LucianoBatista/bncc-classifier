4e2f5e52748bbdc77e5a778fcba4896da119f03b
Luba <luciano.eq@outlook.com>
2022-01-02 07:42:08 -0300

    :sparkles: removing stopwords and put a white space on the place.

--- src/data_preprocessing/cleaning.py
+++ src/data_preprocessing/cleaning.py
@@ -1,4 +1,6 @@
 import re
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 
 def to_lower(text: str) -> str:
@@ -7,15 +9,23 @@ def to_lower(text: str) -> str:
 
 def remove_tags(text: str) -> str:
     pattern = re.compile("<.*?>")
-    cleantext = re.sub(pattern, "", text).replace(u"\xa0", u" ")
+    cleantext = re.sub(pattern, " ", text).replace(u"\xa0", u" ")
     return cleantext
 
 
 def remove_punctuation(text: str) -> str:
-    return re.sub(r"[^\w\s]", "", text)
+    return re.sub(r"[^\w\s]", " ", text)
 
 
 def remove_numbers(text: str) -> str:
     pattern = re.compile("[0-9]+")
-    clean_text = re.sub(pattern, "", text)
+    clean_text = re.sub(pattern, " ", text)
     return clean_text
+
+
+def remove_standard_stopwords(text: str) -> str:
+    stop_words = set(stopwords.words("portuguese"))
+    word_tokens = word_tokenize(text)
+    filtered_sentence = [w for w in word_tokens if not w in stop_words]
+    final_sentence = " ".join(filtered_sentence)
+    return final_sentence
