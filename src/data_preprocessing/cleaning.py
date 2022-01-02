d3c63fb54320140a6e06eb19613ddcf59c924c21
Luba <luciano.eq@outlook.com>
2022-01-02 07:05:33 -0300

    :construction: updating packages, cleaning notebook and cleaning script.

--- src/data_preprocessing/cleaning.py
+++ src/data_preprocessing/cleaning.py
@@ -1,7 +1,21 @@
 import re
 
 
-def remove_tags(text: str):
+def to_lower(text: str) -> str:
+    return text.lower()
+
+
+def remove_tags(text: str) -> str:
     pattern = re.compile("<.*?>")
     cleantext = re.sub(pattern, "", text).replace(u"\xa0", u" ")
     return cleantext
+
+
+def remove_punctuation(text: str) -> str:
+    return re.sub(r"[^\w\s]", "", text)
+
+
+def remove_numbers(text: str) -> str:
+    pattern = re.compile("[0-9]+")
+    clean_text = re.sub(pattern, "", text)
+    return clean_text
