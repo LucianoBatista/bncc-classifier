570b17bddb80a2955746b7b2f43746cdd9655387
Luba <luciano.eq@outlook.com>
2022-03-04 07:53:29 -0300

    refactor: baseline second classifier.

---
+++ analysis/backup_code/luciano-04-03-peu-backup-cleaning.py
@@ -0,0 +1,56 @@
+# #%%
+# # Transforming all of questions to string
+# df_bncc_copy_targets_bncc["questions"] = df_bncc_copy_targets_bncc["questions"].astype(
+#     str
+# )
+# #%%
+# # Removing html tags from questions
+#
+# df_bncc_copy_targets_bncc["questions_tags_free"] = df_bncc_copy_targets_bncc[
+#     "questions"
+# ].apply(html.unescape)
+#
+# df_bncc_copy_targets_bncc.head()
+# #%%
+# # Removing HTML
+# df_bncc_copy_targets_bncc["questions_html_free"] = df_bncc_copy_targets_bncc[
+#     "questions_tags_free"
+# ].apply(lambda text: cleaning.remove_html(text))
+# df_bncc_copy_targets_bncc.head()
+# #%%
+# # Lower Casing
+# df_bncc_copy_targets_bncc["questions_lower"] = df_bncc_copy_targets_bncc[
+#     "questions_html_free"
+# ].str.lower()
+#
+# df_bncc_copy_targets_bncc.head()
+# #%%
+# # Removing punctuation of the questions
+# df_bncc_copy_targets_bncc["questions_punctuation_free"] = df_bncc_copy_targets_bncc[
+#     "questions_lower"
+# ].apply(lambda text: cleaning.remove_punctuation(text))
+# df_bncc_copy_targets_bncc.head()
+# #%%
+# df_bncc_copy_targets_bncc["questions_no_italic_quotes"] = df_bncc_copy_targets_bncc[
+#     "questions_punctuation_free"
+# ].apply(cleaning.remove_italic_quotes)
+# df_bncc_copy_targets_bncc["questions_no_open_quotes"] = df_bncc_copy_targets_bncc[
+#     "questions_no_italic_quotes"
+# ].apply(cleaning.remove_open_quotes)
+# df_bncc_copy_targets_bncc["questions_no_end_quotes"] = df_bncc_copy_targets_bncc[
+#     "questions_no_open_quotes"
+# ].apply(cleaning.remove_end_quotes)
+#
+# df_bncc_copy_targets_bncc["questions_no_italic_dquotes"] = df_bncc_copy_targets_bncc[
+#     "questions_no_end_quotes"
+# ].apply(cleaning.remove_italic_dquotes)
+# df_bncc_copy_targets_bncc["questions_no_open_dquotes"] = df_bncc_copy_targets_bncc[
+#     "questions_no_italic_dquotes"
+# ].apply(cleaning.remove_open_dquotes)
+# df_bncc_copy_targets_bncc["questions_no_end_dquotes"] = df_bncc_copy_targets_bncc[
+#     "questions_no_open_dquotes"
+# ].apply(cleaning.remove_end_dquotes)
+#
+# df_bncc_copy_targets_bncc["questions_no_quotes"] = df_bncc_copy_targets_bncc[
+#     "questions_no_end_dquotes"
+# ].apply(cleaning.remove_quote)
--- src/data_preprocessing/cleaning.py
+++ src/data_preprocessing/cleaning.py
@@ -1,6 +1,16 @@
 import re
+import string
+from collections import Counter
+
+import nltk
+import pandas as pd
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+
+PUNCT_TO_REMOVE = string.punctuation
+nltk.download("stopwords")
+pt_stopwords = set(stopwords.words("portuguese"))
+en_stopwords = set(stopwords.words("english"))
 
 
 def to_lower(text: str) -> str:
@@ -9,7 +19,7 @@ def to_lower(text: str) -> str:
 
 def remove_tags(text: str) -> str:
     pattern = re.compile("<.*?>")
-    cleantext = re.sub(pattern, " ", text).replace(u"\xa0", u" ")
+    cleantext = re.sub(pattern, " ", text).replace("\xa0", " ")
     return cleantext
 
 
@@ -26,6 +36,110 @@ def remove_numbers(text: str) -> str:
 def remove_standard_stopwords(text: str) -> str:
     stop_words = set(stopwords.words("portuguese"))
     word_tokens = word_tokenize(text)
-    filtered_sentence = [w for w in word_tokens if not w in stop_words]
+    filtered_sentence = [w for w in word_tokens if w not in stop_words]
     final_sentence = " ".join(filtered_sentence)
     return final_sentence
+
+
+def remove_html(text):
+    html_pattern = re.compile("<.*?>")
+    return html_pattern.sub(r"", text)
+
+
+def remove_punctuation_2(text):
+    return text.translate(str.maketrans("", "", PUNCT_TO_REMOVE))
+
+
+# Função para remover aspas em itálico
+def remove_italic_dquotes(text: str) -> str:
+    pattern = re.compile(r'"')
+    clean_text = re.sub(pattern, " ", text)
+    return clean_text
+
+
+# Função para remover aspas de abertura
+def remove_open_dquotes(text: str) -> str:
+    pattern = re.compile(r"“")
+    clean_text = re.sub(pattern, " ", text)
+    return clean_text
+
+
+# Função para remover aspas de fechamento
+def remove_end_dquotes(text: str) -> str:
+    pattern = re.compile(r"”")
+    clean_text = re.sub(pattern, " ", text)
+    return clean_text
+
+
+# Função para remover single quotes normal
+def remove_italic_quotes(text: str) -> str:
+    pattern = re.compile(r"'")
+    clean_text = re.sub(pattern, " ", text)
+    return clean_text
+
+
+# Função para remover single quotes abertura
+def remove_open_quotes(text: str) -> str:
+    pattern = re.compile(r"‘")
+    clean_text = re.sub(pattern, " ", text)
+    return clean_text
+
+
+# Função para remover single quotes fechamento
+def remove_end_quotes(text: str) -> str:
+    pattern = re.compile(r"’")
+    clean_text = re.sub(pattern, " ", text)
+    return clean_text
+
+
+# Função para remover aspas de abertura
+def remove_quote(text: str) -> str:
+    pattern = re.compile(r"‛")
+    clean_text = re.sub(pattern, " ", text)
+    return clean_text
+
+
+def remove_pt_stopwords(text):
+    return " ".join([word for word in str(text).split() if word not in pt_stopwords])
+
+
+def remove_en_stopwords(text):
+    return " ".join([word for word in str(text).split() if word not in en_stopwords])
+
+
+class RemoveFrqRare:
+    def __init__(self, df: pd.DataFrame, n_frq_words: int = 10, n_rare_words: int = 10):
+        self.df = df
+        self.n_frq_words = n_frq_words
+        self.n_rare_words = n_rare_words
+        self.frq_words = []
+        self.rare_words = []
+
+    def calc_frq_words(self):
+        cnt = Counter()
+        self.frq_words = set([w for (w, wc) in cnt.most_common(self.n_frq_words)])
+
+    def calc_rare_words(self):
+        cnt = Counter()
+        self.rare_words = set(
+            [w for (w, wc) in cnt.most_common()[: -self.n_rare_words - 1 : -1]]  # noqa
+        )
+
+    def remove_frq_words(self, text: str):
+        return " ".join(
+            [word for word in str(text).split() if word not in self.frq_words]
+        )
+
+    def remove_rare_words(self, text: str):
+        return " ".join(
+            [word for word in str(text).split() if word not in self.rare_words]
+        )
+
+    def remove_frq_and_rare(self):
+        df_to_return = self.df.copy()
+        df_to_return["questions_clean"] = (
+            self.df["questions_clean"]
+            .apply(lambda x: self.remove_frq_words(x))
+            .apply(lambda x: self.remove_rare_words(x))
+        )
+        return df_to_return
